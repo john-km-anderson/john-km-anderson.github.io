08d52f63e185c97ec83a67cf2768bcd8ec4cb4d0
oliver-clarke <38257682+oliver-clarke@users.noreply.github.com>
2018-05-31 20:41:49 -0700

    asfasd

--- stephen-jang/js/box.js
+++ stephen-jang/js/box.js
@@ -1,7 +1,17 @@
-$("#js-rotating").Morphext({
-    animation: "flipInX",
+$("#js-rotating-text").Morphext({
+    animateIn: "flipInX",
+    animateOut: "flipOutX",
     separator: ",",
     speed: 3000,
-    complete: function () {
+    complete: function() {
     }
 });
+
+$("#js-rotating-arrow").Morphist({
+    animateIn: "bounce",
+    animateOut: "bounce",
+    speed: 2000,
+    complete: function() {
+
+    }
+})
